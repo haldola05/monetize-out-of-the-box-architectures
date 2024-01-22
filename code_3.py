4edbc03788438cc0270312c5da1e8594f86db84a
haldola05 <146481065+haldola05@users.noreply.github.com>
2024-01-22 15:21:59 +0100

    Create code_3.py

---
+++ code_3.py
@@ -0,0 +1,41 @@
+}
+}
+    return data;
+function generateRandomData() {
+
+main();
+
+    const data = [];
+    });
+    const data = generateRandomData();
+    for (let i = 0; i < 10; i++) {
+        data.push(Math.floor(Math.random() * 100) + 1);
+    }
+
+    data.forEach(item => {
+function main() {
+        console.log("Random Number: " + item);        }
+}
+        for (int item : data) {
+        for (int i = 0; i < 10; i++) {
+
+            System.out.println("Random Number: " + item);
+        int[] data = new int[10];
+
+            data[i] = (int) (Math.random() * 100) + 1;
+public class RandomDataGenerator {
+    }
+        }
+    public static void main(String[] args) {            data[i] = (int) (Math.random() * 100) + 1;
+
+        }
+public class RandomDataGenerator {
+            System.out.println("Random Number: " + item);
+        for (int item : data) {
+    }
+}
+
+    public static void main(String[] args) {
+        }
+        for (int i = 0; i < 10; i++) {
+        int[] data = new int[10];
